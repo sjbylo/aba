f0db22ac882335e688c482b5b21be3637fdc5fce
Stephen Bylo <steve@localhost.localdomain>
2023-11-22 14:07:54 +0800

    Don't upload the ISO if it's older than the touched file

--- bin/vmw-upload.sh
+++ bin/vmw-upload.sh
@@ -10,13 +10,14 @@ fi
 [ ! "$ISO_DATASTORE" ] && ISO_DATASTORE=$GOVC_DATASTORE
 
 # If the ISO is newer, then upload it
-if [ "$(find $MANEFEST_DIR/agent.x86_64.iso -newer $MANEFEST_DIR/agent.x86_64.iso.uploaded)" ]; then
+###if [ "$(find $MANEFEST_DIR/agent.x86_64.iso -newer $MANEFEST_DIR/agent.x86_64.iso.uploaded)" ]; then
+if [ $MANEFEST_DIR/agent.x86_64.iso -nt $MANEFEST_DIR/agent.x86_64.iso.uploaded ]; then
 	echo Uploading image $MANEFEST_DIR/agent.x86_64.iso to [$ISO_DATASTORE] images/agent-${CLUSTER_NAME}.iso
 
 	if govc datastore.upload -ds $ISO_DATASTORE $MANEFEST_DIR/agent.x86_64.iso images/agent-${CLUSTER_NAME}.iso; then
 		touch $MANEFEST_DIR/agent.x86_64.iso.uploaded 
 	else
-		echo "Warning: ISO file may be attached to a running VM and cannot be overwritten.  Stop the VM first."
+		echo "Warning: ISO file may be attached to a running VM and cannot be overwritten.  Stop the VM first?"
 	fi
 else
 	echo "Image file '$MANEFEST_DIR/agent-${CLUSTER_NAME}.iso' already uploaded to [$ISO_DATASTORE] images/agent-${CLUSTER_NAME}.iso"
