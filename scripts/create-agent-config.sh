6fb75bcb1931ab61f64827a8afabd6896e6b5dc0
Stephen Bylo <stephenbylo@gmail.com>
2023-12-12 11:43:10 +0800

    Fix dig install

--- scripts/create-agent-config.sh
+++ scripts/create-agent-config.sh
@@ -13,11 +13,6 @@ source mirror.conf
 export machine_ip_prefix=$(echo $machine_network | cut -d\. -f1-3).
 export rendezvous_ip=$machine_ip_prefix$starting_ip_index
 
-# Checking if dig is installed ...
-install_rpm bind-utils
-#install_pip j2cli
-
-
 ##echo Validating the cluster configuraiton ...
 
 scripts/verify-config.sh || exit 1
