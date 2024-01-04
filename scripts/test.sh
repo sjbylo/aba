ff7eb8b8a57509eff6e1ec025c34de4b69f973ff
Stephen Bylo <StephenBylo@gmail.com>
2024-01-04 18:47:47 +0800

    Improve testing

--- scripts/test.sh
+++ scripts/test.sh
@@ -35,23 +35,28 @@ cp -f templates/mirror.conf .
 sed -i "s/ocp_target_ver=[0-9]\+\.[0-9]\+\.[0-9]\+/ocp_target_ver=$ver/g" ./mirror.conf
 ####
 
+# test for remote mirror
+sed -i "s/registry.example.com/registry2.example.com/g" ./mirror.conf
+sed -i "s#reg_ssh=#reg_ssh=~/.ssh/id_rsa#g" ./mirror.conf
+# test for remote mirror
+
 ######################
 echo Runtest: START - sync
-make -C mirror mirror-registry
-mirror/mirror-registry uninstall --autoApprove || true
-rm -rf mirror/deps
-rm -rf mirror/save
+#make -C mirror mirror-registry
+#mirror/mirror-registry uninstall --autoApprove || true
+make -C mirror uninstall
 make -C mirror clean
-make sync 
-install_all_clusters sno compact standard 
+make sync   # This will install and sync
+#install_all_clusters sno compact standard 
+install_all_clusters sno
 
 ######################
 echo Runtest: START - load
-make -C mirror mirror-registry
-mirror/mirror-registry uninstall --autoApprove || true
-rm -rf mirror/deps
-rm -rf mirror/save
+#make -C mirror mirror-registry
+#mirror/mirror-registry uninstall --autoApprove || true
+make -C mirror uninstall
 make -C mirror clean
-make save load
-install_all_clusters sno compact standard 
+make load   #  This will save, install, load
+#install_all_clusters sno compact standard 
+install_all_clusters standard
 
