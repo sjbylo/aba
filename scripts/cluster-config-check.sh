e3228499b9bacdfc14c5be8899f2924600d3b446
Stephen Bylo <StephenBylo@gmail.com>
2024-01-10 08:55:35 +0800

    Add extra check

---
+++ scripts/cluster-config-check.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+# This script checks to see if the needed values can be extracted from the agent-config.yaml and the install-config.yaml files.
+
+. scripts/include_all.sh
+
+export MANEFEST_SRC_DIR=.
+
+ICONF=$MANEFEST_SRC_DIR/install-config.yaml  
+ACONF=$MANEFEST_SRC_DIR/agent-config.yaml  
+
+# If one of the files is missing, stop!
+if [ ! -s $ICONF -o ! -s $ACONF ]; then
+	echo "Cannot parse cluster configuration. 'install-config.yaml' and/or 'agent-config.yaml' do not exist."
+	exit 1
+fi
+
