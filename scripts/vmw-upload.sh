5ff763b40d78457c8281aba9bfca7ebc018f2dd8
Stephen Bylo <StephenBylo@gmail.com>
2024-01-18 09:02:40 +0800

    New test case

--- scripts/vmw-upload.sh
+++ scripts/vmw-upload.sh
@@ -23,9 +23,10 @@ fi
 echo Uploading image $MANEFEST_DIR/agent.x86_64.iso to [$ISO_DATASTORE] images/agent-${CLUSTER_NAME}.iso
 
 if govc datastore.upload -ds $ISO_DATASTORE $MANEFEST_DIR/agent.x86_64.iso images/agent-${CLUSTER_NAME}.iso; then
-	touch $MANEFEST_DIR/agent.x86_64.iso.uploaded 
+	###touch $MANEFEST_DIR/agent.x86_64.iso.uploaded 
+	:
 else
-	echo "Warning: ISO file may be attached to a running VM and cannot be overwritten.  Stop the VM first?"
+	echo "Warning: ISO file may be attached to a running VM and cannot be overwritten.  Try to stop the VM first with 'make stop'."
 fi
 
 echo
