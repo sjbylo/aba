e7fcef24b343aad48838308ec5bd600877b72985
Stephen Bylo <StephenBylo@gmail.com>
2024-01-21 18:52:42 +0800

    Add dep. for RH pull-secret

--- scripts/install-pull-secret.sh
+++ scripts/install-pull-secret.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-[ -s ~/.pull-secret.json ] && exit 0
+[ -s ~/.pull-secret.json ] && echo "Red Hat pull secret found at ~/.pull-secret.json" && exit 0
 
-echo -n "Paste the pull secret and then hit Ctrl-D: "
-umask 077
-cat > ~/.pull-secret.json
+echo
+echo "Warning: Please write your Red Hat registry pull secret to the file ~/.pull-secret.json."
+echo "         Fetch your secret key from https://console.redhat.com/openshift/downloads#tool-pull-secret"
+echo
+
+exit 1
+
+# FIXME: Should we rather do this? ...
+#echo -n "Paste the pull secret and then hit Ctrl-D: "
+#umask 077
+#cat > ~/.pull-secret.json
 
