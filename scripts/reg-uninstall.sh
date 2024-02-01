d5e545c13bccd113abf030358edb4d76390050ed
Stephen Bylo <StephenBylo@gmail.com>
2024-02-01 17:32:31 +0800

    make 'uninstall' a bit more clever!

--- scripts/reg-uninstall.sh
+++ scripts/reg-uninstall.sh
@@ -18,26 +18,28 @@ if [ -s reg-uninstall.sh ]; then
 	rm -f ./reg-uninstall.sh
 	exit 0
 else
+	echo
 	echo "Warning: No uninstall script 'mirror/reg-uninstall.sh' found."
 	echo "If Aba did not install the mirror registry, then uninstall manually. Return to continue, Ctrl-C to abort." 
-	read yn
+	echo
+	sleep 5
+	#read yn
 fi
 
 # FIXME: Is this still needed? 
 # FIXME, only the uninstall script needed?
-#if [ "$reg_ssh" ]; then
+if [ "$reg_ssh" ] && ssh $(whoami)@reg_host podman ps | grep registry; then
+	echo "Running command: ./mirror-registry uninstall -v --targetHostname $reg_host --targetUsername $(whoami) -k $reg_ssh $reg_root_opt"
+	./mirror-registry uninstall -v \
+		--targetHostname $reg_host \
+  		--targetUsername $(whoami) \
+		--autoApprove \
+  		-k $reg_ssh $reg_root_opt
+elif podman ps | grep registry; then
+		echo "Running command: ./mirror-registry uninstall -v $reg_root_opt"
+		./mirror-registry uninstall -v --autoApprove $reg_root_opt
+else
+	echo No mirror registry to uninstall
+fi
 
-#	echo "Running command: ./mirror-registry uninstall -v --targetHostname $reg_host --targetUsername $(whoami) -k $reg_ssh $reg_root_opt"
-#	./mirror-registry uninstall -v \
-#		--targetHostname $reg_host \
-#  		--targetUsername $(whoami) \
-#		--autoApprove \
-#  		-k $reg_ssh $reg_root_opt
-#else
-#	echo "Running command: ./mirror-registry uninstall -v $reg_root_opt"
-#	./mirror-registry uninstall -v --autoApprove $reg_root_opt
-#fi
-
-# If there is no uninstall script, assume nothing to uninstall
-#echo No mirror registry to uninstall
 echo
