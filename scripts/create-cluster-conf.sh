15b43839985ef9ed19d92676f8a554a36fdf1ec5
Stephen Bylo <StephenBylo@gmail.com>
2024-02-07 19:35:39 +0800

    Renamed aba.conf to cluster.conf and added aba/aba.conf as a global config file.

---
+++ scripts/create-cluster-conf.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash 
+# Script to set up the cluster.conf file
+
+. scripts/include_all.sh
+
+source aba.conf
+
+if [ ! "$ocp_version" ]; then
+	echo "Please run ./aba first!"
+	exit 1
+fi
+
+if [ "$1" ]; then
+	cd $1 || exit 1
+	scripts/j2 templates/cluster-$1.conf > cluster.conf 
+else
+	scripts/j2 templates/cluster.conf > cluster.conf 
+	$editor cluster.conf
+fi
+
+exit 0
+
