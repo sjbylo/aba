6fe7acdd351dfaa33ad793e5ed9a4d1568e3f5c1
Stephen Bylo <StephenBylo@gmail.com>
2024-02-09 16:53:01 +0800

    Improved rpms d/l

---
+++ scripts/install-rpms.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+rpms="podman make jq bind-utils nmstate net-tools skopeo python3 python3-jinja2 python3-pyyaml openssl"
+
+for rpm in $rpms
+do
+	rpm -q --quiet $rpm && continue
+
+	echo "Installing required rpms $rpms ..."
+	sudo dnf install $rpms -y >> .dnf-install.log 2>&1
+	echo "Installed rpms"
+	return 
+done
+
