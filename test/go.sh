963d39bf6ba24b5808a104c55252fcdb7037f0d7
Stephen Bylo <StephenBylo@gmail.com>
2024-02-22 13:36:06 +0800

    Fixed 'wrong CLI version' installed bug

--- test/go.sh
+++ test/go.sh
@@ -8,7 +8,7 @@ cd ~/aba
 export target_full=1   # Build vm
 #export target_full=    # Build only iso
 rm -f [0-9]-stage
-(
+time (
 	> test/test.log
 	touch test/0-stage
 	test/test1-basic-sync-test-and-save-load-test.sh && \
@@ -18,3 +18,4 @@ rm -f [0-9]-stage
 	test/test5-airgapped-install-local-reg.sh && \
 	touch test/3-stage
 ) && echo SUCCESS | tee test/test.log || echo FAILED | tee test/test.log
+date 
