4736551c6afd8c85bdb285336c9a8f5206d62f40
Stephen Bylo <StephenBylo@gmail.com>
2024-02-23 17:39:17 +0800

    Added agent config file test

--- test/test1-basic-sync-test-and-save-load-test.sh
+++ test/test1-basic-sync-test-and-save-load-test.sh
@@ -66,7 +66,7 @@ sed -i "s#reg_ssh=#reg_ssh=~/.ssh/id_rsa#g" ./mirror/mirror.conf	     	# Remote 
 #sed -i "s#reg_pw=.*#reg_pw=             #g" ./mirror/mirror.conf	    	# test random password 
 ### sed -i "s#tls_verify=true#tls_verify=            #g" ./mirror/mirror.conf  	# test tlsverify = false # sno install fails 
 ### sed -i "s#reg_port=.*#reg_pw=443             #g" ./mirror/mirror.conf	    	# test port change
-#sed -i "s#reg_path=.*#reg_path=mypath             #g" ./mirror/mirror.conf	    	# test path
+#sed -i "s#reg_path=.*#reg_path=my/path             #g" ./mirror/mirror.conf	    	# test path
 
 mylog Install mirror 
 
@@ -76,26 +76,65 @@ source <(cd mirror; normalize-mirror-conf)
 
 mylog "Mirror available at $reg_host:$reg_port"
 
-
-#test-cmd() {
-#	echo "$*"
-#	eval "$*"
-#	echo done
-#}
-
-#test-cmd 'ssh 10.0.1.6 -- "echo $PWD; hostname"'
-
 ######################
 test-cmd 'make -C mirror sync'   # This will install mirror and sync
 
-if true; then
-	rm -rf sno
-	test-cmd make sno $targetiso
-else
-	rm -rf sno
-	test-cmd make sno 
-	test-cmd make -C sno delete 
-fi
+# Install yq!
+which yq || (
+	mylog Install yq
+	curl -sSL -o - https://github.com/mikefarah/yq/releases/download/v4.41.1/yq_linux_amd64.tar.gz | tar -C ~/bin -xzf - ./yq_linux_amd64 && \
+		mv ~/bin/yq_linux_amd64 ~/bin/yq && \
+		chmod 755 ~/bin/yq
+	)
+
+######################
+######################
+# This test creates the ABI (agent-based installer) config files to check they are valid
+
+for cname in sno compact standard
+do
+	mkdir -p test/$cname
+
+        echo "Agent-config file generation test for cluster type '$cname'"
+
+        rm -rf $cname
+
+        test-cmd "make $cname target=cluster.conf"
+        sed -i "s#mac_prefix=.*#mac_prefix=88:88:88:88:88:#g" $cname/cluster.conf   # make sure all mac addr are the same, not random
+        test-cmd "make -C $cname install-config.yaml"
+        test-cmd "make -C $cname agent-config.yaml"
+
+	# There are only run on the very first run to generate the valis files
+        [ ! -f test/$cname/install-config.yaml.example ] && cat $cname/install-config.yaml | yq 'del(.additionalTrustBundle,.platform.vsphere.vcenters,.pullSecret)' > test/$cname/install-config.yaml.example
+        [ ! -f test/$cname/agent-config.yaml.example ]   && cat $cname/agent-config.yaml   > test/$cname/agent-config.yaml.example
+
+	# Remove some of the params which either change or cannot be placed into git (FIXME: specify the VC password exactly) 
+	cat $cname/install-config.yaml | yq 'del(.additionalTrustBundle,.platform.vsphere.vcenters,.pullSecret)' > test/$cname/install-config.yaml
+	cat $cname/agent-config.yaml                                                                             > test/$cname/agent-config.yaml
+
+        # Check if the files DO NOT match (are different)
+        if ! diff test/$cname/install-config.yaml test/$cname/install-config.yaml.example; then
+		cp test/$cname/install-config.yaml test/$cname/install-config.yaml.failed
+                exit 1
+        fi
+
+        if ! diff test/$cname/agent-config.yaml   test/$cname/agent-config.yaml.example; then
+		cp test/$cname/agent-config.yaml test/$cname/agent-config.yaml.failed
+                exit 1
+        fi
+
+        test-cmd "make -C $cname iso"
+        #test-cmd "make -C $cname delete"
+done
+
+######################
+######################
+
+
+######################
+rm -rf sno
+test-cmd make sno $targetiso
+test-cmd make -C sno delete || true
 
 #######################
 test-cmd "make -C mirror save load"  #  This will save, install then load
@@ -118,7 +157,7 @@ test-cmd make -C mirror uninstall
 
 mylog Various mirror tests:
 
-mylog Confgure mirror to install on remote bastion2 in ~/my-quay-mirror, with random password to /mypath 
+mylog Confgure mirror to install on remote bastion2 in ~/my-quay-mirror, with random password to /my/path 
 
 #sed -i "s/registry.example.com/registry2.example.com/g" ./mirror/mirror.conf	# Install on registry2 
 #sed -i "s#reg_ssh=#reg_ssh=~/.ssh/id_rsa#g" ./mirror/mirror.conf	     	# Remote or localhost
@@ -126,7 +165,7 @@ mylog Confgure mirror to install on remote bastion2 in ~/my-quay-mirror, with ra
 sed -i "s#reg_root=#reg_root=~/my-quay-mirror#g" ./mirror/mirror.conf	     	# test other storage location
 sed -i "s#reg_pw=.*#reg_pw=             #g" ./mirror/mirror.conf	    	# test random password 
 ### sed -i "s#tls_verify=true#tls_verify=            #g" ./mirror/mirror.conf  	# test tlsverify = false # sno install fails 
-sed -i "s#reg_path=.*#reg_path=mypath             #g" ./mirror/mirror.conf	    	# test path
+sed -i "s#reg_path=.*#reg_path=my/path             #g" ./mirror/mirror.conf	    	# test path
 
 test-cmd make -C mirror install 
 
