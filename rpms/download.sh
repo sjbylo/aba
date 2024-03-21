1ad6e15992a177b1c9ed1b489006778b1cd463a3
Stephen Bylo <StephenBylo@gmail.com>
2024-03-21 11:59:13 +0800

    Added notes and --alldeps option

--- rpms/download.sh
+++ rpms/download.sh
@@ -1,10 +1,17 @@
 #!/bin/bash -e
+# Download the required rpms (which match *this hosts* version) to this directory.
+# The version of your external and internal bastions needs to be the same for this to work
+# Later, 'make tar' will include these rpm files into the archive, to be installed on the internal bastion
+# Note, if the bastions are not identicle (same rpms installed) try using --alldeps option 
 
 source /etc/os-release
 
 [ ! "$REDHAT_SUPPORT_PRODUCT_VERSION" ] && echo "Cannot determine the host OS version" && exit 1
 
+# Override the version here, if needed
 #REDHAT_SUPPORT_PRODUCT_VERSION=8
 
 rpm -q dnf-utils --quiet || sudo yum install yum-utils -y
 sudo yumdownloader --releasever=$REDHAT_SUPPORT_PRODUCT_VERSION --resolve $(cat ../templates/rpms-internal.txt) --destdir=.
+#sudo yumdownloader --alldeps --releasever=$REDHAT_SUPPORT_PRODUCT_VERSION --resolve $(cat ../templates/rpms-internal.txt) --destdir=.
+
