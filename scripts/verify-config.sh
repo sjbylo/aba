31f6bb02c9c1f8fa5b52f9872101b2d4abfbcb48
Stephen Bylo <StephenBylo@gmail.com>
2024-03-27 15:39:37 +0800

    UX polish

--- scripts/verify-config.sh
+++ scripts/verify-config.sh
@@ -19,13 +19,13 @@ SNO=
 [ $num_masters -ne 1 -a $num_masters -ne 3 ] && echo "Error: number of masters can only be '1' or '3'" && exit 1
 echo "Master count is valid [$num_masters]"
 
-#echo Checking SNO config ...
-if [ "$SNO" -a $num_masters -eq 1 -a $num_workers -ne 0 ]; then
-	echo "Error: number of workers must be '0' if number of masters is '1 (SNO)"
+# Checking for invalid config 
+if [ $num_masters -eq 1 -a $num_workers -ne 0 ]; then
+	echo "Error: number of workers must be '0' if number of masters is '1' (SNO)"
 	exit 1
-else
-	echo "SNO master and worker count is valid [$num_masters] [$num_workers]"
 fi
+
+echo "Master [$num_masters] and worker counts [$num_workers] are valid."
 
 # If not SNO, then ensure api_vip and ingress_vip are defined 
 #echo Checking api_vip and ingress_vip are defined ...
@@ -33,34 +33,35 @@ if [ ! "$SNO" ]; then
 	[ ! "$api_vip" -o ! "$ingress_vip" ] && echo "Error: 'api_vip' and 'ingress_vip' must be defined for this configuration" && exit 1
 	echo "'api_vip' and 'ingress_vip' are defined [$api_vip] [$ingress_vip]"
 else
-	[ "$api_vip" -o "$ingress_vip" ] && echo "'api_vip' and 'ingress_vip' are defined. No need for SNO, they will be ignored." 
+	[ "$api_vip" -o "$ingress_vip" ] && echo "Warning: No need for 'api_vip' and 'ingress_vip' values in SNO configuration, they will be ignored." 
 fi
 
 # Checking if dig is installed ...
 
-ip_api=$(dig +time=8 +short api.$cluster_name.$base_domain)
-ip_apps=$(dig +time=8 +short x.apps.$cluster_name.$base_domain)
-	
+ip_of_api=$(dig +time=8 +short api.$cluster_name.$base_domain)
+ip_of_apps=$(dig +time=8 +short x.apps.$cluster_name.$base_domain)
+
 # If NOT SNO...
 if [ ! "$SNO" ]; then
-	# Ensure api DNS exists 
-	[ "$ip_api" != "$api_vip" ] && echo "WARNING: DNS record [api.$cluster_name.$base_domain] does not resolve to [$api_vip]" && exit 1
-	echo "DNS record for api is valid [$ip_api]"
+	# Ensure api DNS exists and points to correct ip
+	[ "$ip_of_api" != "$api_vip" ] && echo "ERROR: DNS record [api.$cluster_name.$base_domain] does not resolve to [$api_vip]" && exit 1
+	echo "DNS record for api (api.$cluster_name.$base_domain) is valid [$ip_of_api]"
 
-	# Ensure apps DNS exists 
-	[ "$ip_apps" != "$ingress_vip" ] && echo "WARNING: DNS record [\*.apps.$cluster_name.$base_domain] does not resolve to [$ingress_vip]" && exit 1
-	echo "DNS record for ingress is valid [$ip_apps]"
+	# Ensure apps DNS exists and points to correct ip
+	[ "$ip_of_apps" != "$ingress_vip" ] && echo "ERROR: DNS record [\*.apps.$cluster_name.$base_domain] does not resolve to [$ingress_vip]" && exit 1
+	echo "DNS record for ingress (*.apps.$cluster_name.$base_domain) is valid [$ip_of_apps]"
 else
-	# Check values are pointing to "rendezvous_ip"
+	# For SNO...
+	# Check values are both pointing to "rendezvous_ip"
 	# Ensure api DNS exists 
-	[ "$ip_api" != "$rendezvous_ip" ] && \
-		echo "WARNING: DNS record [api.$cluster_name.$base_domain] does not resolve to the rendezvous ip [$rendezvous_ip]" && exit 1
-	echo "DNS record for api (SNO) is valid [$ip_api]"
+	[ "$ip_of_api" != "$rendezvous_ip" ] && \
+		echo "ERROR: DNS record [api.$cluster_name.$base_domain] does not resolve to the rendezvous ip [$rendezvous_ip]" && exit 1
+	echo "DNS record for api (SNO) is valid [$ip_of_api]"
 
 	# Ensure apps DNS exists 
-	[ "$ip_apps" != "$rendezvous_ip" ] && \
-		echo "WARNING: DNS record [\*.apps.$cluster_name.$base_domain] does not resolve to the rendezvous ip [$rendezvous_ip]" && exit 1
-	echo "DNS record for ingress (SNO) is valid [$ip_apps]"
+	[ "$ip_of_apps" != "$rendezvous_ip" ] && \
+		echo "ERROR: DNS record [\*.apps.$cluster_name.$base_domain] does not resolve to the rendezvous ip [$rendezvous_ip]" && exit 1
+	echo "DNS record for ingress (SNO) is valid [$ip_of_apps]"
 fi
 
 exit 0
