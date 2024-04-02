3dfb2c60aa80de84def68148740cd5a2b5f29e6e
Stephen Bylo <StephenBylo@gmail.com>
2024-04-02 16:33:11 +0800

    Added --insecure-skip-tls-verify

--- scripts/show-cluster-login.sh
+++ scripts/show-cluster-login.sh
@@ -1,3 +1,5 @@
 #!/bin/bash 
 
-echo "oc login -u kubeadmin -p '$(cat iso-agent-based/auth/kubeadmin-password)' $(cat iso-agent-based/auth/kubeconfig | grep server | awk '{print $NF}')"
+[ ! -d iso-agent-based ] && echo "Cluster not installed.  Run 'make' to install the cluster or 'make iso' to create the iso file." && exit 1
+
+echo "oc login -u kubeadmin -p '$(cat iso-agent-based/auth/kubeadmin-password)' --insecure-skip-tls-verify $(cat iso-agent-based/auth/kubeconfig | grep server | awk '{print $NF}')"
