4c6fc5f555bf0e957a839c127dc65e3e3159b02e
Stephen Bylo <StephenBylo@gmail.com>
2024-04-02 14:45:26 +0800

    UX

--- scripts/ssh-rendezvous.sh
+++ scripts/ssh-rendezvous.sh
@@ -4,6 +4,7 @@ source scripts/include_all.sh
 
 source <(normalize-cluster-conf) 
 
+[ ! -f iso-agent-based/rendezvousIP ] && echo "Error: iso-agent-based/rendezvousIP file missing.  Run 'make' or 'make iso' to create it." && exit 1
 ip=$(cat iso-agent-based/rendezvousIP)
 
 if [ "$*" ]; then
