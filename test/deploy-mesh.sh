e8bce67a6994a22a9a59482409a16bfc7c5e7480
Stephen Bylo <StephenBylo@gmail.com>
2024-04-14 22:05:16 +0800

    Comment only

--- test/deploy-mesh.sh
+++ test/deploy-mesh.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -e
+# This test script is run on the remote internal bastion only
 
 export KUBECONFIG=$PWD/aba/sno/iso-agent-based/auth/kubeconfig
 
 cd ~/aba/test/mesh/openshift-service-mesh-demo
 
+# Use this simple method.   The "redirect methods" are problematic.
 sed -i "s/quay\.io/$reg_host:$reg_port/g" */*.yaml */*/*.yaml */*/*/*.yaml
 
 echo "y" | ./00-install-all.sh
