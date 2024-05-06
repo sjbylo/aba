4ebe44e95c0387402d977f08f80467bf9047f737
Stephen Bylo <StephenBylo@gmail.com>
2024-05-06 17:55:12 +0800

    Backup manifest dir fix

--- scripts/generate-image.sh
+++ scripts/generate-image.sh
@@ -7,23 +7,26 @@ echo ==================================================================
 echo Cluster configuration
 echo =====================
 scripts/cluster-config.sh | sed "s/export /  /g"  | sed -e "s/=\"/=/g" -e "s/\"$//g"| tr "=" " " | column -t 
-#scripts/cluster-config.sh | sed "s/^/  /g"  | sed -e "s/=\"/=/g" -e "s/\"$//g"| tr "=" " " | column -t 
 echo ==================================================================	
 eval `scripts/cluster-config.sh || exit 1`
 
+if [ -d $MANEFEST_DIR ]; then
+	echo "Backup up previous $MANEFEST_DIR' dir to '$MANEFEST_DIR.backup':"
+
+	rm -rf $MANEFEST_DIR.backup
+	cp -rp $MANEFEST_DIR $MANEFEST_DIR.backup
+fi
+
 echo Generating the ISO boot image for cluster: $CLUSTER_NAME.$BASE_DOMAIN ...
 
-rm -rf iso-agent-based 
-mkdir -p iso-agent-based
+rm -rf $MANEFEST_DIR 
+mkdir -p $MANEFEST_DIR
 
 cp install-config.yaml agent-config.yaml $MANEFEST_DIR 
-
-rm -rf $MANEFEST_DIR.backup
-cp -rp $MANEFEST_DIR $MANEFEST_DIR.backup
 
 echo "openshift-install agent create image --dir $MANEFEST_DIR "
 openshift-install agent create image --dir $MANEFEST_DIR 
 
 echo 
-echo "The agent based ISO has been created in the 'iso-agent-based' directory"
+echo "The agent based ISO has been created in the '$MANEFEST_DIR' directory"
 echo
