d58937c1b449ca0b186629423d41da3eabb8df9e
Stephen Bylo <StephenBylo@gmail.com>
2024-06-23 18:35:18 +0800

    Changed flow, if platform=vmw, no need to ask

--- scripts/install-vmware.conf.sh
+++ scripts/install-vmware.conf.sh
@@ -9,29 +9,26 @@ source scripts/include_all.sh
 # Needed for $editor
 source <(normalize-aba-conf)
 
+[ "$platform" != "vmw" ] && echo "Platform var not set to 'vmw'. Not configuring 'vmware.conf'." && > vmware.conf && exit 0
+
 if [ ! -s vmware.conf ]; then
 	echo
-	if ask "Install OpenShift onto vSphere or ESXi (access from the private network is required!)?"; then
+	## Use 'platform' env var: if ask "Install OpenShift onto vSphere or ESXi (access from the private network is required!)?"; then
 
-	#echo "Install OpenShift onto vSphere or ESXi (access from the private network is required!)?"
-#	echo -n "Hit Return to edit the 'vmware.conf' file or 'n' to skip (Y/n): " 
-
-#	read yn
-
-#	if [ "$yn" = "y" -o "$yn" = "" ]; then
-		if [ -s ~/.vmware.conf ]; then
-			echo "Creating 'vmware.conf' from '~/.vmware.conf'"
-			cp ~/.vmware.conf vmware.conf   # The working user edited file, if any
-		else
-			echo "Creating 'vmware.conf' from 'templates/vmware.conf'"
-			cp templates/vmware.conf .  # The default template 
-		fi
-		[ "$ask" ] && $editor vmware.conf 
+	if [ -s ~/.vmware.conf ]; then
+		echo "Creating 'vmware.conf' from '~/.vmware.conf'"
+		cp ~/.vmware.conf vmware.conf   # The working user edited file, if any
 	else
-		echo "Creating empty 'vmware.conf' file.  To use vSphere or ESXi, delete the file and run 'make vmw'."
-		> vmware.conf
-		exit 0
+		echo "Creating 'vmware.conf' from 'templates/vmware.conf'"
+		cp templates/vmware.conf .  # The default template 
 	fi
+	[ "$ask" ] && $editor vmware.conf 
+
+		## Use 'platform' env var: else
+		## Use 'platform' env var: echo "Creating empty 'vmware.conf' file.  To use vSphere or ESXi, delete the file and run 'make vmw'."
+		## Use 'platform' env var: > vmware.conf
+		## Use 'platform' env var: exit 0
+	## Use 'platform' env var: fi
 fi
 
 source <(normalize-vmware-conf)
