d77ead873c4f8ad5a7ee48c46a391ce246c215bf
Stephen Bylo <StephenBylo@gmail.com>
2024-07-05 21:12:37 +0800

    Re-wording only

--- scripts/setup-cluster.sh
+++ scripts/setup-cluster.sh
@@ -29,17 +29,17 @@ echo "Creating '$name/cluster.conf' file for cluster type [$cluster_type]."
 scripts/create-cluster-conf.sh $name $cluster_type
 
 msg="Install the cluster with 'cd $name; make'?"
-[ "$target" ] && msg="Trigger the cluster with 'cd $name; make $target'?"
+[ "$target" ] && msg="Make the target '$target' with 'cd $name; make $target'?"
 
 if ask $msg; then
 	make $target
 
 	echo 
-	[ "$target" ] && echo "If you want to continue working on this cluster, change into the directory '$name'. Example: 'cd $name && make help'"
+	[ "$target" ] && echo "To continue working on this cluster, change into the directory '$name'. Example: 'cd $name && make help'"
 	echo
 else
 	echo 
-	echo "If you want to continue working on this cluster, change into the directory '$name' and run 'make'.  Example: 'cd $name && make' or 'cd $name && make help'"
+	echo "To continue working on this cluster, change into the directory '$name' and run 'make'.  Example: 'cd $name && make' or 'cd $name && make help'"
 	echo
 fi
 
