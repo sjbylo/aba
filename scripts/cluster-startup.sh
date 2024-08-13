4a65a0283202e5fe07fd286ab15ecdacf7eec216
Stephen Bylo <StephenBylo@gmail.com>
2024-08-13 08:53:18 +0800

    Improved

--- scripts/cluster-startup.sh
+++ scripts/cluster-startup.sh
@@ -1,12 +1,18 @@
 #!/bin/bash -e
 # Start up the cluster.  Need to uncordon to allow pods to run again.
 
+[ ! -d iso-agent-based ] && echo "Cluster not installed!" && exit 1
+server_url=$(cat iso-agent-based/auth/kubeconfig | grep server | awk '{print $NF}' | head -1)
+
 echo Starting cluster ...
 make -s start || exit 1
 
-#oc whoami >/dev/null 2>&1 || (echo Waiting for cluster startup; sleep 40) 
 echo Waiting for cluster startup ...
-sleep 40
+# Or use: timeout 3 bash -c "</dev/tcp/host/6443"
+while ! curl -skI $server_url >/dev/null
+do
+	sleep 2
+done
 
 # Use one of the methods to access the cluster
 echo Attempting to log into the cluster ...
@@ -18,18 +24,8 @@ done
 
 sleep 5
 
-#if ! oc whoami >/dev/null 2>&1; then
-#	echo -n Waiting for the cluster to start ...
-#	sleep 60
-#	until oc whoami >/dev/null 2>&1 >/dev/null
-#	do
-#		echo -n .
-#		sleep 10
-#	done
-#	sleep 20
-#fi
-
 cluster_id=$(oc whoami --show-server | awk -F[/:] '{print $4}')
+echo
 echo Cluster $cluster_id nodes:
 echo
 oc get nodes
