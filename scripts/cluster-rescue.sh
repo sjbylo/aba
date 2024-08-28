4fa03d68c05270c04507d9ccac3529894e53c8eb
Stephen Bylo <StephenBylo@gmail.com>
2024-08-28 13:43:20 +0800

    Added cluster rescue script (2)

--- scripts/cluster-rescue.sh
+++ scripts/cluster-rescue.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
 
-source scripts/include_all.sh 
-
-source <(normalize-cluster-conf) 
-
+# Prep execution on one node
 if [ ! "$1" = "--exec" ]; then
+	source scripts/include_all.sh 
+	source <(normalize-cluster-conf) 
+
 	# This will run locally and will copy and exec the rescue script (below)
 	if [ ! -f iso-agent-based/rendezvousIP ]; then
 		echo_red "Error: iso-agent-based/rendezvousIP file missing.  Run 'make' or 'make iso' to create it."
@@ -12,20 +12,25 @@ if [ ! "$1" = "--exec" ]; then
 		exit 1
 	fi
 
-	scp $0 -i $ssh_key_file core@$ip:
-	ssh -i $ssh_key_file core@$ip -- sudo bash $(basename $0) --exec
+	ip=$(cat iso-agent-based/rendezvousIP)
+
+	scp -i $ssh_key_file $0 core@$ip:
+	ssh -i $ssh_key_file    core@$ip -- sudo bash $(basename $0) --exec
 
 	exit $?
 fi
 
+# Now rescue the cluster ...
 export KUBECONFIG=/etc/kubernetes/static-pod-resources/kube-apiserver-certs/secrets/node-kubeconfigs/localhost-recovery.kubeconfig
 
-# let pods exec again, if needed
 if oc get nodes | grep -q SchedulingDisabled; then
+	echo "Setting nodes scheduling enabled"
 	for node in $(oc get nodes -o jsonpath='{.items[*].metadata.name}'); do oc adm uncordon ${node} & done
+else
+	echo "No nodes set to 'SchedulingDisabled'"
 fi
 
-# accept any pending CSRs 
+echo "Checking if any CSRs to accept ..."
 until [ ! "$(oc get csr -o go-template='{{range .items}}{{if not .status}}{{.metadata.name}}{{"\n"}}{{end}}{{end}}')" ]
 do
 	oc get csr -o go-template='{{range .items}}{{if not .status}}{{.metadata.name}}{{"\n"}}{{end}}{{end}}' | \
@@ -33,6 +38,7 @@ do
 
 	sleep 20
 done
+echo "Done"
 
 sleep 20
 
