93a435e57d1a7d4d4ce74b51f6cdde03503ce503
Stephen Bylo <StephenBylo@gmail.com>
2024-10-05 07:42:07 +0800

    Fix for checking public pull secret when it's ot needed

--- scripts/create-install-config.sh
+++ scripts/create-install-config.sh
@@ -123,7 +123,7 @@ fi
 
 # Check that the release image is available in the private registry
 if [ "$additional_trust_bundle" -a "$image_content_sources" ]; then
-	scripts/create-containers-auth.sh
+	scripts/create-containers-auth.sh --load
 	scripts/verify-release-image.sh
 fi
 
--- test/go.sh
+++ test/go.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 # Simple top level script to run all tests
 
-export VER_OVERRIDE=4.16.12
+export VER_OVERRIDE=4.16.12 # Uncomment to use the 'latest' stable version of OCP
 
 # This is for testing a specific branch ($1) directly from "git clone", otherwise it will test
 # the local dir. ($PWD)
