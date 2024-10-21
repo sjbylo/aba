e956f94299987855a27b79739c92a358c7acd193
Stephen Bylo <StephenBylo@gmail.com>
2024-10-21 10:18:14 +0800

    Changed name of flag file

--- scripts/check-cluster-installed.sh
+++ scripts/check-cluster-installed.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
-# If the .finished flag file exists, refuse to continue
+# If the .install-complete flag file exists, refuse to continue
 
 source scripts/include_all.sh
 
-[ -f .finished ] && \
+[ -f .install-complete ] && \
 	echo_magenta "This cluster has already been deployed successfully!" && \
-	echo_magenta "Run 'make clean; make' to re-install the cluster or remove the '.finished' flag file and try again." && exit 1
+	echo_magenta "Run 'make clean; make' to re-install the cluster or remove the '.install-complete' flag file and try again." && exit 1
 
 exit 0
