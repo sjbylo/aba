4f7ddd1379dc9ac7ead578aca73d42ee00f3b07f
Stephen Bylo <StephenBylo@gmail.com>
2024-10-26 17:13:22 +0800

    fix and ensure always ask

--- scripts/distclean-gate.sh
+++ scripts/distclean-gate.sh
@@ -8,8 +8,11 @@ if [ "$force" ]; then
 	exit 0
 else
 	# The default answer is "no" (-n).  So, No = exit 1, Yes = exit 0
-	ask -n "This is the same as a factory reset and will delete all files! Are you sure" && exit 1 || exit 0
+	# Always ask!
+	echo_cyan -n "This is the same as a factory reset of the aba repository and will delete all files! Are you sure? (y/N): " ## && exit 1 || exit 0
+	read yn
+	[ "$yn" = "y" -o "$yn" = "Y" ] && exit 0
 fi
 
-exit 0
+exit 1
 
