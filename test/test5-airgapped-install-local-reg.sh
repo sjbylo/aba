5e66995d515e1e5d5ad7d862161e073a02d8aea4
Stephen Bylo <StephenBylo@gmail.com>
2024-11-30 12:38:15 +0800

    Fix test

--- test/test5-airgapped-install-local-reg.sh
+++ test/test5-airgapped-install-local-reg.sh
@@ -172,6 +172,8 @@ test-cmd -h $reg_ssh_user@$int_bastion -r 5 3 -m "Checking regcreds/ does not ex
 ######################
 mylog Runtest: START - airgap
 
+test-cmd -h $reg_ssh_user@$int_bastion -r 20 3 -m  "Install aba script" "cd $subdir/aba; ./install" 
+
 test-cmd -h $reg_ssh_user@$int_bastion -r 20 3 -m  "Loading cluster images into mirror on internal bastion" "cd $subdir/aba; aba load" 
 
 test-cmd -h $reg_ssh_user@$int_bastion -m  "Delete already loaded image set file to make space: '$subdir/aba/mirror/save/mirror_seq1_000000.tar'" "rm -f $subdir/aba/mirror/save/mirror_seq1_000000.tar" 
