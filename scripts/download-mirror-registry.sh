33ca2a4d9d744627fbf71d14cf773e0a1674b710
Stephen Bylo <StephenBylo@gmail.com>
2024-12-08 19:33:20 +0800

    Start to allow oc for rhel8 or rhel 9 to be d/l

--- scripts/download-mirror-registry.sh
+++ scripts/download-mirror-registry.sh
@@ -1,7 +1,8 @@
 #!/bin/bash 
-# Dpwnload the most latest version of mirror-registry
+# Download the latest version of mirror-registry
 
 ver=$(curl -skL https://mirror.openshift.com/pub/openshift-v4/clients/mirror-registry/ | grep -oP '>(\d+\.\d+(\.\d+)?)<' | grep -oP '\d+\.\d+(\.\d+)?' | sort -V | tail -n 1)
 [ ! "$ver" ] && ver=1.3.11
+echo Downloading https://developers.redhat.com/content-gateway/rest/mirror/pub/openshift-v4/clients/mirror-registry/$ver/mirror-registry.tar.gz ...
 curl --progress-bar -OL https://developers.redhat.com/content-gateway/rest/mirror/pub/openshift-v4/clients/mirror-registry/$ver/mirror-registry.tar.gz
 
