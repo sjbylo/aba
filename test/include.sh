1c05d758af84deedce121e84279d64c19792b71e
Stephen Bylo <StephenBylo@gmail.com>
2025-02-13 14:32:51 +0800

    cache fix

--- test/include.sh
+++ test/include.sh
@@ -324,9 +324,9 @@ END
 	###test-cmd -h testy@$int_bastion_hostname -m "Delete and create sub dir on remote host for user $u" "rm -rf $subdir && mkdir $subdir"
 	if [ "$test_user" = "root" ]; then
 		# /root does not have enough space by default, so we link it to ~steve/root
-		test-cmd -h $test_user@$int_bastion_hostname -m "Create sub dir on remote host for $test_user" "rm -vrf $subdir && mkdir -vp ~steve/root/subdir && ln -vfs ~steve/root/subdir && ls -l subdir"
-		test-cmd -h $test_user@$int_bastion_hostname -m "Create dir on remote host for oc-mirror cache ($test_user)" "rm -vrf ~/.oc-mirror && mkdir -vp ~steve/root/.oc-mirror && ln -vfs ~steve/root/.oc-mirror && ls -l .oc-mirror"
-		test-cmd -h $test_user@$int_bastion_hostname -m "Create 2nd dir on remote host for oc-mirror cache ($test_user)" "mkdir -p quay-install && ln -vfs ~steve/.cashe quay-install && ls -l quay-install/.cashe"
+		test-cmd -h $test_user@$int_bastion_hostname -m "Create sub dir on remote host for $test_user" "rm -vrf $subdir && mkdir -vp ~steve/root/subdir && ln -vs ~steve/root/subdir && ls -l subdir"
+		test-cmd -h $test_user@$int_bastion_hostname -m "Create dir on remote host for oc-mirror cache ($test_user)" "mkdir -vp ~steve/root/.oc-mirror && ln -vs ~steve/root/.oc-mirror && ls -l .oc-mirror"
+		test-cmd -h $test_user@$int_bastion_hostname -m "Create 2nd dir on remote host for oc-mirror cache ($test_user)" "mkdir -p ~steve/root/quay-install && ln -vs ~steve/root/quay-install && ls -l quay-install"
 	else
 		test-cmd -h $test_user@$int_bastion_hostname -m "Create sub dir on remote host for $test_user" "rm -vrf $subdir && mkdir -v $subdir"
 	fi
