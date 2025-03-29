d5e12ed0570ed19b29a0d25bed90a31b95b5f152
Stephen Bylo <StephenBylo@gmail.com>
2025-03-29 23:16:36 +0800

    Added support to install OSUS, connect to mirror and adjust clusterversion

---
+++ scripts/day2-config-osus.sh
@@ -0,0 +1,129 @@
+#!/bin/bash -e
+
+source scripts/include_all.sh
+
+[ "$1" ] && set -x
+
+umask 077
+
+source <(normalize-aba-conf)
+source <(normalize-cluster-conf)
+source <(normalize-mirror-conf)
+
+verify-aba-conf || exit 1
+verify-cluster-conf || exit 1
+verify-mirror-conf || exit 1
+
+NAME=sample
+NAMESPACE=openshift-update-service
+
+oc apply -f - <<END
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: $NAMESPACE
+  annotations:
+    openshift.io/node-selector: ""
+  labels:
+    openshift.io/cluster-monitoring: "true"
+---
+apiVersion: operators.coreos.com/v1
+kind: OperatorGroup
+metadata:
+  name: update-service-operator-group
+  namespace: $NAMESPACE
+spec:
+  targetNamespaces:
+  - $NAMESPACE
+  upgradeStrategy: Default
+---
+apiVersion: operators.coreos.com/v1alpha1
+kind: Subscription
+metadata:
+  name: update-service-subscription
+  namespace: $NAMESPACE
+spec:
+  channel: v1
+  installPlanApproval: "Automatic"
+  source: "redhat-operators"
+  sourceNamespace: "openshift-marketplace"
+  name: "cincinnati-operator"
+END
+
+
+csv_cmd="oc get subscription -n $NAMESPACE update-service-subscription -o jsonpath='{.status.installedCSV}'"
+CSV=$(eval $csv_cmd)
+until [ "$CSV" ]
+do
+	echo -n .
+	sleep 10
+	CSV=$(oc get subscription -n $NAMESPACE update-service-subscription -o jsonpath='{.status.installedCSV}')
+	CSV=$(eval $csv_cmd)
+done
+
+echo CSV=$CSV
+
+while ! oc get csv -n $NAMESPACE $CSV -o jsonpath='{.status.phase}' | grep Succeeded 
+do
+	echo -n .
+	sleep 10
+done
+
+graph_image=$reg_host:$reg_port/$reg_path/openshift/graph-image:latest
+release_repo=$reg_host:$reg_port/$reg_path/openshift/release-images
+
+echo graph_image=$graph_image
+echo release_repo=$release_repo
+
+oc apply -f - <<END
+apiVersion: updateservice.operator.openshift.io/v1
+kind: UpdateService
+metadata:
+  name: $NAME
+  namespace: $NAMESPACE
+spec:
+  graphDataImage: "$graph_image"
+  releases: "$release_repo"
+  replicas: 1
+END
+
+
+if [ -s regcreds/rootCA.pem ]; then
+        ca_cert="$(cat regcreds/rootCA.pem | sed ':a;N;$!ba;s/\n/\\n/g')"
+	#c=$(echo "$cart" | sed "s/\\n/\\\n/g"); echo $c
+        echo "Using root CA file at regcreds/rootCA.pem"
+	kubectl patch configmap registry-config -n openshift-config --type='merge' -p '{"data":{"updateservice-registry":"'"$ca_cert"'"}}'
+fi
+
+echo "Obtaining the policy engine route ..."
+
+while sleep 1; do POLICY_ENGINE_GRAPH_URI="$(oc -n "${NAMESPACE}" get -o jsonpath='{.status.policyEngineURI}/api/upgrades_info/v1/graph{"\n"}' updateservice "${NAME}")"; SCHEME="${POLICY_ENGINE_GRAPH_URI%%:*}"; if test "${SCHEME}" = http -o "${SCHEME}" = https; then break; fi; done
+
+echo POLICY_ENGINE_GRAPH_URI=$POLICY_ENGINE_GRAPH_URI
+
+. <(aba shell)
+
+#CH=$(kubectl get clusterversion version -o jsonpath='{.status.desired.version}')-$(kubectl get clusterversion version -o jsonpath='{.spec.channel}')
+CH=$(kubectl get clusterversion version -o jsonpath='{.spec.channel}')
+echo CH=$CH
+
+echo "Checking access to $POLICY_ENGINE_GRAPH_URI/?channel=$CH ..."
+
+#while true; do HTTP_CODE="$(curl -k --header Accept:application/json --output /dev/stderr --write-out "%{http_code}" "${POLICY_ENGINE_GRAPH_URI}?channel=$CH")"; if test "${HTTP_CODE}" -eq 200; then break; fi; echo "${HTTP_CODE}"; sleep 10; done
+while true; do HTTP_CODE="$(curl -k --header Accept:application/json --output /dev/stderr --write-out "%{http_code}" "${POLICY_ENGINE_GRAPH_URI}?channel=$CH")"; if test "${HTTP_CODE}" -eq 200; then break; fi; echo -n .; sleep 10; done
+
+####
+
+ingress_cert="$(oc get secret -n openshift-ingress-operator router-ca -o jsonpath="{.data['tls\.crt']}"| base64 -d | sed ':a;N;$!ba;s/\n/\\n/g')"
+ca_bundle_crt=$(oc get cm user-ca-bundle -n openshift-config -o jsonpath='{.data.ca-bundle\.crt}' | sed ':a;N;$!ba;s/\n/\\n/g')
+ca_bundle_crt="$ca_bundle_crt\n$ingress_cert"
+
+echo ============
+echo "$ca_bundle_crt"
+echo ============
+
+oc patch cm user-ca-bundle -n openshift-config --type='merge' -p '{"data":{"ca-bundle.crt":"'"$ca_bundle_crt"'"}}'
+oc patch proxy cluster --type=merge -p '{"spec":{"trustedCA":{"name":"user-ca-bundle"}}}'
+
+
+
