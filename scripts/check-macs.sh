5da9150f62aaa7736e925faf778c404e6410f3b5
Stephen Bylo <StephenBylo@gmail.com>
2025-04-14 20:52:01 +0800

    Removed need for tmp_dir var

--- scripts/check-macs.sh
+++ scripts/check-macs.sh
@@ -17,28 +17,40 @@ WKR_MAC_ADDR_ARRAY=($WKR_MAC_ADDR)
 which arp >/dev/null || exit 0
 
 # Checking mac addresses that could already be in use (in arp cache) ...
-arp -an > $tmp_dir/.all_arp_entries 
+#arp -an > $tmp_dir/.all_arp_entries 
+all_arp_entries=$(arp -an)
+
 IN_ARP_CACHE=
-> $tmp_dir/.list_of_matching_arp_entries
+#> $tmp_dir/.list_of_matching_arp_entries
+list_of_matching_arp_entries=
 for mac in $CP_MAC_ADDR $WKR_MAC_ADDR
 do
 	# checking mac address: $mac ...
-	if grep -q " $mac " $tmp_dir/.all_arp_entries; then
+	#if grep -q " $mac " $tmp_dir/.all_arp_entries; then
+	if echo "$all_arp_entries" | grep -q " $mac "; then
 		echo "Warning: Mac address $mac might already be in use (found in system ARP cache)."
 		IN_ARP_CACHE=1
-		echo $mac >> $tmp_dir/.list_of_matching_arp_entries
+		#echo $mac >> $tmp_dir/.list_of_matching_arp_entries
+		# If the list is empty, add the mac address, else append the mac addr.
+		[ "$list_of_matching_arp_entries" ] && list_of_matching_arp_entries="${$list_of_matching_arp_entries}"$'\n'"$mac" || list_of_matching_arp_entries=$mac
+		# my_list="${my_list}"$'\n'"dragonfruit"
 	fi
 done
 
 # Checking ip and mac addresses *currently* in use by clearing the cache ...
-> $tmp_dir/.mac_list_filtered
-if [ -s $tmp_dir/.list_of_matching_arp_entries ]; then
-	for mac in `cat $tmp_dir/.list_of_matching_arp_entries`
+#> $tmp_dir/.mac_list_filtered
+mac_list_filtered=
+#if [ -s $tmp_dir/.list_of_matching_arp_entries ]; then
+if [ "$list_of_matching_arp_entries" ]; then
+	#for mac in `cat $tmp_dir/.list_of_matching_arp_entries`
+	for mac in $list_of_matching_arp_entries
 	do
-		grep $mac $tmp_dir/.mac_list_filtered
+		#grep $mac $tmp_dir/.mac_list_filtered
+		echo "$mac_list_filtered" | grep $mac
 	done
 
-	ips=$(cat $tmp_dir/.mac_list_filtered | cut -d\( -f2 | cut -d\) -f1)
+	#ips=$(cat $tmp_dir/.mac_list_filtered | cut -d\( -f2 | cut -d\) -f1)
+	ips=$(echo "$mac_list_filtered" | cut -d\( -f2 | cut -d\) -f1)
 
 	if [ "$ips" ]; then
 		# Delete arp cache and refresh IPs with ping...
@@ -50,13 +62,15 @@ if [ -s $tmp_dir/.list_of_matching_arp_entries ]; then
 		wait 
 		sleep 2
 
-		arp -an > $tmp_dir/.all_arp_entries 
+		#arp -an > $tmp_dir/.all_arp_entries 
+		all_arp_entries=$(arp -an)
 		P=
 		MAC_IN_USE=
 		for mac in $CP_MAC_ADDR $WKR_MAC_ADDR
 		do
 			# checking $mac ...
-			if grep -q " $mac " $tmp_dir/.all_arp_entries; then
+			#if grep -q " $mac " $tmp_dir/.all_arp_entries; then
+			if echo "$all_arp_entries" | grep -q " $mac "; then
 				P="$P $mac"
 				MAC_IN_USE=1
 			fi
