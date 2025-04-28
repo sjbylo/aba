698850845adc7f761b5b2dd27fd4daac31750ce6
Stephen Bylo <StephenBylo@gmail.com>
2025-04-28 17:43:15 +0800

    Fix for missing arch in install-config.yaml

--- scripts/include_all.sh
+++ scripts/include_all.sh
@@ -7,8 +7,9 @@ which sudo 2>/dev/null >&2 && SUDO=sudo
 
 # Set up the arch vars
 export arch_sys=$(uname -m)
-[ "$arch_sys" = "aarch64" ]	&& export arch_short=arm64  # ARM
-[ "$arch_sys" = "x86_64" ]	&& export arch_short=amd64   # Intel
+export arch_short=amd64
+[ "$arch_sys" = "aarch64" -o "$arch_sys" = "arm64"] && export arch_short=arm64  # ARM
+#[ "$arch_sys" = "x86_64" ]	&& export arch_short=amd64   # Intel
 
 echo_black()	{ [ "$TERM" ] && tput setaf 0; echo -e "$@"; [ "$TERM" ] && tput sgr0; }
 echo_red()	{ [ "$TERM" ] && tput setaf 1; echo -e "$@"; [ "$TERM" ] && tput sgr0; }
