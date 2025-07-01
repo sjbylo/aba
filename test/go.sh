5e29bf2ee5a7db2f3b6dd4a3257d7ed7ca3f42db
Stephen Bylo <StephenBylo@gmail.com>
2025-07-01 14:38:45 +0800

    Set oc-mirror to 'latest'

--- test/go.sh
+++ test/go.sh
@@ -22,8 +22,8 @@ export VER_OVERRIDE=l # Uncomment to use the 'latest' stable version of OCP
 export internal_bastion_rhel_ver=rhel9
 #export internal_bastion_rhel_ver=rhel8
 
-export TEST_CHANNEL=stable
-#export TEST_CHANNEL=fast
+export TEST_CHANNEL=fast
+#export TEST_CHANNEL=stable
 #export TEST_CHANNEL=candidate  # This only works if e.g. 4.19.0 is available, does not yet work for release candidate versions e.g. 4.19.0.rc#
 
 export TEST_USER=$(whoami)   # This can be any user or $(whoami) 
