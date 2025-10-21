6a121646763bd7ab0af350226037d08422fb99dd
Stephen Bylo <StephenBylo@gmail.com>
2025-10-21 23:00:18 +0800

    Test fix

--- test/reg-test-uninstall-remote.sh
+++ test/reg-test-uninstall-remote.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 # Install mirror reg remotly but using ssh, simulating "existing reg. use-case.
 
+cd `dirname $0`
+
 cat > .ssh.conf <<END
 StrictHostKeyChecking no
 UserKnownHostsFile=/dev/null
@@ -11,6 +13,10 @@ END
 reg_host=$1
 
 reg_ssh_user=$TEST_USER
+
+rsync --progress --partial -avz reg-install.sh mirror-registry-amd64.tar.gz  $reg_ssh_user@$reg_host:test
+
+ssh -F .ssh.conf $reg_ssh_user@$reg_host tar -C test -xvzf test/mirror-registry-amd64.tar.gz
 
 ssh -F .ssh.conf $reg_ssh_user@$reg_host "cd test && ./mirror-registry uninstall --autoApprove"
 
