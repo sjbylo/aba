c9e087c9dd5b7224ede0b1cdda6a5bcc003f4158
Stephen Bylo <StephenBylo@gmail.com>
2025-10-22 10:08:24 +0800

    Test fix - simulate conf file edit instead

--- test/test5-airgapped-install-local-reg.sh
+++ test/test5-airgapped-install-local-reg.sh
@@ -154,8 +154,8 @@ aba -d mirror mirror.conf
 
 mylog "Test the internal bastion ($int_bastion_hostname) as mirror"
 make -sC mirror mirror.conf force=yes
-test-cmd -m "Setting reg_host=$int_bastion_hostname" aba -d mirror -H $int_bastion_hostname # THis will INSTALL mirror!??? # FIXME?
-#sed -i "s/registry.example.com/$int_bastion_hostname /g" ./mirror/mirror.conf
+#test-cmd -m "Setting reg_host=$int_bastion_hostname" aba -d mirror -H $int_bastion_hostname # THis will INSTALL mirror!??? # FIXME?
+sed -i "s/registry.example.com/$int_bastion_hostname /g" ./mirror/mirror.conf
 
 # This is also a test that overriding vakues works ok, e.g. this is an override in the mirror.connf file, overriding from aba.conf file
 #test-cmd -m "Setting op_sets='abatest' in mirror/mirror.conf" "sed -i 's/^.*op_sets=.*/op_sets=abatest /g' ./mirror/mirror.conf"
