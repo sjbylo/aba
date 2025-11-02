4c58252ef102d9a752c7bc7be5d636f5aa0e92ac
Stephen Bylo <StephenBylo@gmail.com>
2025-11-02 20:19:36 +0800

    Remove all tests using 'aba inc'

--- scripts/aba.sh
+++ scripts/aba.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Start here, run this script to get going!
 
-ABA_VERSION=20251102144948
+ABA_VERSION=20251102201935
 # Sanity check
 echo -n $ABA_VERSION | grep -qE "^[0-9]{14}$" || { echo "ABA_VERSION in $0 is incorrect [$ABA_VERSION]! Fix the format to YYYYMMDDhhmmss and try again!" >&2 && exit 1; }
 
