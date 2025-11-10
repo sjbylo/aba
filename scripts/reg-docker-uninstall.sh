236ef9e2646d0a2c19123302385428a290b22037
Stephen Bylo <StephenBylo@gmail.com>
2025-11-10 17:46:37 +0800

    Fix for unknown var error.  No need for the var.

--- scripts/reg-docker-uninstall.sh
+++ scripts/reg-docker-uninstall.sh
@@ -28,31 +28,19 @@ set -e
 
 # --- Configurable defaults ---
 REGISTRY_NAME="registry"
-INTERNAL_PORT=5000
-EXTERNAL_PORT="${EXTERNAL_PORT:-8443}"
+
+#INTERNAL_PORT=5000
+#EXTERNAL_PORT="${EXTERNAL_PORT:-8443}"
 REGISTRY_DATA_DIR="$data_dir/docker-reg/data"
-REGISTRY_CERTS_DIR=".docker-certs"
-REGISTRY_AUTH_DIR=".docker-auth"
-
-REGISTRY_USER="${REGISTRY_USER:-init}"
-REGISTRY_PASS="${REGISTRY_PASS:-p4ssw0rd}"
-
-# --- Helper functions ---
-echo_green() { echo -e "\033[1;32m$*\033[0m"; }
-echo_yellow() { echo -e "\033[1;33m$*\033[0m"; }
-echo_red() { echo -e "\033[1;31m$*\033[0m"; }
-
-fail_checkp() {
-    echo_red "❌ $1"
-}
-
-pass_check() {
-    echo_green "✅ $1"
-}
+#REGISTRY_CERTS_DIR=".docker-certs"
+#REGISTRY_AUTH_DIR=".docker-auth"
 
 if $DOCKER ps -a --format '{{.Names}}' | grep -q "^${REGISTRY_NAME}$"; then
     echo_yellow "Stopping and removing old registry container..."
     $DOCKER rm -f "$REGISTRY_NAME" || true
 fi
+
+# FIXME: should ask for permission?
+sudo rm -rf $(dirname $REGISTRY_DATA_DIR)
 
 exit 0
