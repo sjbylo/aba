880551f84a29ea089ca979bc6c0edb991bfd2e07
Stephen Bylo <StephenBylo@gmail.com>
2026-02-10 19:43:09 +0800

    Test fix (2)

--- test/misc/test_ssh_if.sh
+++ test/misc/test_ssh_if.sh
@@ -1,3 +1,3 @@
-aba --dir "$1" --cmd "ip a"
-until aba --dir "$1" --cmd "ip a" | grep "$2"; do sleep 10; done
-aba --dir "$1" --cmd "ip a"
+aba --dir "$1" ssh --cmd "ip a"
+until aba --dir "$1" ssh --cmd "ip a" | grep "$2"; do sleep 10; done
+aba --dir "$1" ssh --cmd "ip a"
--- test/misc/test_ssh_ntp.sh
+++ test/misc/test_ssh_ntp.sh
@@ -1,4 +1,4 @@
 d=$1; shift
-aba --dir $d --cmd 'chronyc sources'
-until aba --dir $d --cmd 'chronyc sources' | grep "$*"; do echo -n .; sleep 10; done
-aba --dir $d --cmd 'chronyc sources'
+aba --dir $d ssh --cmd 'chronyc sources'
+until aba --dir $d ssh --cmd 'chronyc sources' | grep "$*"; do echo -n .; sleep 10; done
+aba --dir $d ssh --cmd 'chronyc sources'
