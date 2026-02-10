b1d243213cb4dd23361d2962444995f79c59414b
Stephen Bylo <StephenBylo@gmail.com>
2026-02-10 08:07:49 +0800

    Try to make these tests more robust

--- test/misc/test_ssh_if.sh
+++ test/misc/test_ssh_if.sh
@@ -1,1 +1,3 @@
-until aba --dir "$1" ssh --cmd "ip a" | grep "$2"; do sleep 10; done
+aba --dir "$1" -F ~/.aba/ssh.conf --cmd "ip a"
+until aba --dir "$1" -F ~/.aba/ssh.conf --cmd "ip a" | grep "$2"; do sleep 10; done
+aba --dir "$1" -F ~/.aba/ssh.conf --cmd "ip a"
