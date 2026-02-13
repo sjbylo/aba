ba34776c724e2b5aa1c44a2754f5bb5216214abe
Stephen Bylo <StephenBylo@gmail.com>
2026-02-13 15:30:21 +0800

    Fix for sporadic breaking ssh tests

--- test/misc/test_ssh_if.sh
+++ test/misc/test_ssh_if.sh
@@ -1,3 +1,10 @@
+echo "Showing 'ip a' output:"
 aba --dir "$1" ssh --cmd "ip a"
+
+echo "Waiting for string: '$2':"
 until aba --dir "$1" ssh --cmd "ip a" | grep "$2"; do sleep 10; done
+
+echo "Showing 'ip a' output:"
 aba --dir "$1" ssh --cmd "ip a"
+
+exit 0
