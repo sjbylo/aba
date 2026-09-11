138ba06d62179f6587c5578bc415d5f28d005be1
Stephen Bylo <StephenBylo@gmail.com>
2026-09-11 08:49:37 +0800

    github action update

--- scripts/aba.sh
+++ scripts/aba.sh
@@ -23,7 +23,7 @@
 ABA_VERSION=1.2.4
 
 # Build timestamp (updated by build/pre-commit-checks.sh)
-ABA_BUILD=20260911084217
+ABA_BUILD=20260911084936
 
 # Sanity check version and build timestamp at startup
 # FIXME: Can only use 'echo' here since can't locate the include_all.sh file yet
