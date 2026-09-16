5ffc7bcf5c60ff9176e57628f8878f7706fb16d5
Stephen Bylo <StephenBylo@gmail.com>
2026-09-16 09:18:06 +0800

    Update catalogs

--- scripts/aba.sh
+++ scripts/aba.sh
@@ -23,7 +23,7 @@
 ABA_VERSION=1.3.2
 
 # Build timestamp (updated by build/pre-commit-checks.sh)
-ABA_BUILD=20260913092855
+ABA_BUILD=20260916091804
 
 # Sanity check version and build timestamp at startup
 # FIXME: Can only use 'echo' here since can't locate the include_all.sh file yet
